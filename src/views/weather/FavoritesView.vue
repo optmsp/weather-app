99fdfe7154e5c0cc88874070ee760995a2c7003f
Dustin Puryear <dustin70879@gmail.com>
2025-01-01 10:19:38 -0600

    break out the search results from the favorites.

--- src/views/weather/FavoritesView.vue
+++ src/views/weather/FavoritesView.vue
@@ -33,7 +33,7 @@ const toggleFavorite = () => {
 
     <CitySearch @search="handleSearch" @use-current-location="handleUseCurrentLocation" />
 
-    <div class="favorites-grid">
+    <div class="search-results-grid">
       <div v-if="loading" class="loading-container">
         <span class="loading loading-spinner loading-lg"></span>
       </div>
@@ -48,18 +48,20 @@ const toggleFavorite = () => {
           :weather="currentWeather"
           @toggle-favorite="toggleFavorite"
         />
-
-        <template v-if="favorites.length > 0">
-          <h2 class="section-header">Favorites</h2>
-          <WeatherCard
-            v-for="weather in favorites"
-            :key="weather.location"
-            :weather="weather"
-            @toggle-favorite="() => appStore.toggleFavorite(weather)"
-          />
-        </template>
       </template>
     </div>
+
+    <template v-if="favorites.length > 0">
+      <h2 class="section-header">Favorites</h2>
+      <div class="favorites-grid">
+        <WeatherCard
+          v-for="weather in favorites"
+          :key="weather.location"
+          :weather="weather"
+          @toggle-favorite="() => appStore.toggleFavorite(weather)"
+        />
+      </div>
+    </template>
   </div>
 </template>
 
@@ -73,6 +75,10 @@ const toggleFavorite = () => {
 }
 
 .favorites-grid {
+  @apply grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6;
+}
+
+.search-results-grid {
   @apply grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6;
 }
 
